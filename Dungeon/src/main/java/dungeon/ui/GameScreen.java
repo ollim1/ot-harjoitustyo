15485afca3ee4ba025baa9be5ca77603e4f31033
Olli Määttä <ollimaatta@gmail.com>
2019-04-22 17:47:37 +0300

    disabled pathfinder debugging

--- Dungeon/src/main/java/dungeon/ui/GameScreen.java
+++ Dungeon/src/main/java/dungeon/ui/GameScreen.java
@@ -70,7 +70,7 @@ public class GameScreen {
                 if (game.isGameOver()) {
                     gameOver();
                 }
-                updateDebug();
+                update();
             }
         });
     }
